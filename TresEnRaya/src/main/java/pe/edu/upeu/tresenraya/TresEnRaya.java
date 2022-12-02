e61ca0e44bace5b61594a59f8bd1439dcee14301
jaqueline21623 <jaqueline.huahuaccpa@upeu.edu.pe>
2022-12-02 08:04:25 -0500

    Evaluacion

---
+++ TresEnRaya/src/main/java/pe/edu/upeu/tresenraya/TresEnRaya.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package pe.edu.upeu.tresenraya;
+
+/**
+ *
+ * @author DELL
+ */
+public class TresEnRaya {
+
+    public static void main(String[] args) {
+        new ventana().setVisible(true);
+    }
+}
